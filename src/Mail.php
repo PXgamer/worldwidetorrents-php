e090718574406cc00e2fc8a03003ecf0110a28d9
PXgamer <owzie123@gmail.com>
2017-01-03 15:47:54 +0000

    Added Mail class for mailing stats and in future, PMs via the API

---
+++ src/Mail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace pxgamer\WorldWideTorrents;
+
+/**
+ * Class Mail
+ * @package pxgamer\WorldWideTorrents
+ */
+class Mail
+{
+    public $client;
+
+    /**
+     * Mail constructor.
+     * @param Client|null $client
+     */
+    public function __construct(Client $client = null)
+    {
+        if (is_null($client)) {
+            $client = new Client();
+        }
+        $this->client = $client;
+
+        if ($this->client->authKey) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param null $userId
+     * @return array|bool|mixed
+     */
+    public function getStats($userId = null)
+    {
+        if (!is_numeric($userId)) {
+            return false;
+        }
+
+        return $this->client->get('/mail/?id=' . $userId);
+    }
+}
