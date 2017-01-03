453a4ac4e8d10646522bae9f1aff22b5d32b2c97
PXgamer <owzie123@gmail.com>
2017-01-03 15:47:12 +0000

    Added User class for getting information about a user and their torrents

---
+++ src/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace pxgamer\WorldWideTorrents;
+
+/**
+ * Class User
+ * @package pxgamer\WorldWideTorrents
+ */
+class User
+{
+    public $client;
+
+    /**
+     * User constructor.
+     * @param Client|null $client
+     */
+    public function __construct(Client $client = null)
+    {
+        if (is_null($client)) {
+            $client = new Client();
+        }
+        $this->client = $client;
+
+        if ($this->client->authKey) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param null $userId
+     * @return array|bool|mixed
+     */
+    public function getInfo($userId = null)
+    {
+        if (!is_numeric($userId)) {
+            return false;
+        }
+
+        return $this->client->get('/user/?id='.$userId);
+    }
+
+    /**
+     * @param null $userId
+     * @return array|bool|mixed
+     */
+    public function getTorrents($userId = null)
+    {
+        if (!is_numeric($userId)) {
+            return false;
+        }
+
+        return $this->client->get('/user/torrents/?id='.$userId);
+    }
+}
